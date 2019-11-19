fe6e23e35e6eed085b3a092d2b46f41f89b05c1d
NEHA <NEHA@192.168.1.207>
2019-11-19 17:44:49 +0530

    First Commit

---
+++ HelloWorld/src/com/csi/oops/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.csi.oops;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10, b=20;
+		System.out.println("\n Addition is : "+(a+b));
+		System.out.println("\n Hello World");
+	}
+
+}
---
+++ HelloWorld/src/com/csi/oops/RotateLinkedList.java
@@ -0,0 +1,12 @@
+package com.csi.oops;
+
+public class RotateLinkedList {
+	public static void foo() {
+		throw new RuntimeException();
+		
+	}
+	public static void main(String[] args) {
+		foo();
+	}
+
+}
